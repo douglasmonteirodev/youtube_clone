f810eb9d0f6194583648435948edbabe467959ad
Douglas Monteiro <douglasm.dev@gmail.com>
2022-09-17 15:13:24 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import Feed from "./components/Feed";
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+import SearchFeed from "./components/SearchFeed";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <HashRouter>
+      <div className='container'>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
+};
+
+export default App;
