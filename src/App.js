d824f522ca2dc852031a0cfc72bc91a8a85aa797
Douglas Monteiro <douglasm.dev@gmail.com>
2022-09-21 18:08:48 -0300

    upload paste build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Feed from "./components/Feed";
 import VideoDetail from "./components/VideoDetail";
